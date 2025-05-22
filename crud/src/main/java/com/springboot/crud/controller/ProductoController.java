2bdb39e5c9382c14e107efeba0a2241c3ad18615
Alejandra <alejandrasandoval4321@gmail.com>
2025-05-22 10:26:02 -0600

    Cambios en productos

--- crud/src/main/java/com/springboot/crud/controller/ProductoController.java
+++ crud/src/main/java/com/springboot/crud/controller/ProductoController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/productos")
+@RequestMapping("/productoss")
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
